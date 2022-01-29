3a91e32925be2b0fdd8cc649dd38259d294352e2
tonyclem <tonyclem001@gmail.com>
2022-01-29 22:54:48 +0100

    added string sections to knows the length of the string

---
+++ string-length.js
@@ -0,0 +1,6 @@
+// Here we decleared a variable called mySentence
+
+let mySentence = "Programming is so interesting!";
+
+// we use length to find the length of any sentence or words
+console.log(mySentence.length); // 30
