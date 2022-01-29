0974b5d032d483d1a06a17f01b54671583d91cb2
tonyclem <tonyclem001@gmail.com>
2022-01-29 22:47:30 +0100

    added log-animals and gitignire

---
+++ log-animal.js
@@ -0,0 +1,28 @@
+//  Declearing a variable called animalsInForest, but we assigned it to and empty array, so let see what next ?
+
+let animalsInForest = [];
+
+// Here we hava to write a lit note about our arrays
+console.log(
+  "we're about to create an arrrays, of animals, the names of wilde animals in forest"
+);
+
+//  here we write a console log that log our arrays
+console.log(animalsInForest);
+
+// Here we have to create a new variable called favoriteAnimal,  name of our favorite animals in this new array
+let favoriteAnimal = ["Lion", "Tiger", "Elephant"];
+
+// Here we write a console log is going to log on the second animals
+console.log(favoriteAnimal);
+
+// we about to add another animal to our animals array
+console.log(
+  "here we need to use push method to assign another animals to our array objects"
+);
+
+favoriteAnimal.push("Piglet");
+
+// Here we console log our favorite animals
+
+console.log(favoriteAnimal);
